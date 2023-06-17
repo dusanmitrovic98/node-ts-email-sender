1ea7fa384f905fe1f182f4123f71db634df1e1a9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-17 22:44:38 +0200

    Update app.ts

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -1,16 +1,1 @@
 import nodemailer, {Trasporter } from 'nodemailer';
-import { ReadStream, createReadStream } from 'fs';
-import { promisify } from 'util';
-
-function app(): void {
-  const transporter: Transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-password',
-    },
-  });
-  
-  const readFileAsync = promisify(createReadStream);
-  
-  async function sendEmailWithAttachments(
