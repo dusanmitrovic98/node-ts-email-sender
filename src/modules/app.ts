05b826a077c79f591f5e62a7287096352d304f38
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-18 22:17:40 +0200

    Update app.ts

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -1,11 +1,1 @@
 import nodemailer, {Trasporter } from 'nodemailer';
-import { ReadStream, createReadStream } from 'fs';
-import { promisify } from 'util';
-
-function app(): void {
-  const transporter: Transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-password',
-    },
